a819bc6f028e789c28b88a7b61084c077a1622fc
dangnhon <dang.nhon12345@yahoo.com>
2021-02-04 09:39:05 -0800

    my counter does work >:(

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,48 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleChangeName = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  handleChangePass = (e) => {
+    this.setState({
+      password: e.target.value 
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    // if (this.state.password.length && this.state.username.length > 0) {
+    //   this.props.handleLogin({username: this.state.username, password: this.state.password})
+    // } else {
+    //   alert("fill out both")
+    // } 
+
+    this.state.password.length && this.state.username.length > 0 ? this.props.handleLogin({username: this.state.username, password: this.state.password}) : alert("fill out both")
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={(e) => this.handleSubmit(e)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={(e) => this.handleChangeName(e)} value={this.state.username} id="username" name="username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={(e) => this.handleChangePass(e)} value={this.state.password} id="password" name="password" type="password" />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "test"
+    };
   }
+
+  handleOnChange = (e) => {
+    this.setState({
+      message: e.target.value 
+    })    
+  }
+
+  
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={(e) => this.handleOnChange(e)} value={this.state.message} type="text" name="message" id="message" />
+        <p>{this.props.maxChars - this.state.message.length}</p>
       </div>
     );
   }
